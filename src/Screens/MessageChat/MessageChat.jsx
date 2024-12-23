430eca4d2bbf7bb9d2d621243433f5b13fabe6c4
23eugecio <eugeciotti@gmail.com>
2024-12-23 16:39:37 -0300

    css

--- src/Screens/MessageChat/MessageChat.jsx
+++ src/Screens/MessageChat/MessageChat.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Send, ArrowLeft } from 'lucide-react';
 import { useNavigate } from 'react-router-dom';
-import '../GlobalStyle/GlobalStyle.css';
+import './MessageChat.css';
 import { getUnnauthenticatedHeaders, POST } from '../../Fetching/http.fetching';
 
 
