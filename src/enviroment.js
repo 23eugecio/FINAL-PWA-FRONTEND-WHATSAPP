b3dfbbd2f20d20ac2356911c080b55cee773c1bd
23eugecio <eugeciotti@gmail.com>
2024-12-23 17:36:57 -0300

    css

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,7 +1,7 @@
 
 const ENVIROMENT = {
     'URL_BACK': 'https://proyecto-final-back-end-olive.vercel.app',
-    'URL_FRONT': 'https://final-pwa-frontend-whatsapp.vercel.app'
+    'URL_FRONT': 'https://localhost:3000'
 }
 
 
