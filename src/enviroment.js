18c1d75594bdab7d0cde9b6b912639a41c78300d
23eugecio <eugeciotti@gmail.com>
2024-12-23 22:35:13 -0300

    css

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,7 +1,7 @@
 
 const ENVIROMENT = {
-    'URL_BACK': 'https://localhost:3000',
-    'URL_FRONT': 'https://localhost:5173'
+    'URL_BACK': 'https://vercel.com/maria-eugenia-ciottis-projects/proyecto-final-back-end',
+    'URL_FRONT': 'https://vercel.com/maria-eugenia-ciottis-projects/final-pwa-frontend-whatsapp-8989'
 }
 
 
