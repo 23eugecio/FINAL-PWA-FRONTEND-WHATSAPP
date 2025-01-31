984c1edaa9a59f02cbda2aa628da2018046b1e53
23eugecio <eugeciotti@gmail.com>
2025-01-31 15:17:31 -0300

    redeploy

--- src/enviroment.js
+++ src/enviroment.js
@@ -1,7 +1,7 @@
 
 const ENVIROMENT = {
     URL_BACK: 'https://proyecto-final-back-end-olive.vercel.app',
-    URL_FRONT: 'https://final-pwa-frontend-whatsapp-8989.vercel.app'
+    URL_FRONT: 'https://final-pwa-frontend-wh-git-328738-maria-eugenia-ciottis-projects.vercel.app'
 }
 
 
